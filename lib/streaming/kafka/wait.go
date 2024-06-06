ac60db326f3f2a12a94a597ce4aea80477104bc8
Hannes Hansen <mail@hannesh.de>
2024-06-06 13:13:45 +0200

    fixed kafka url

--- lib/streaming/kafka/wait.go
+++ lib/streaming/kafka/wait.go
@@ -10,7 +10,7 @@ import (
 
 func WaitForKafkaMessageReceived(topic, regexMsg string, sendFnc func(context.Context) error, timeout time.Duration, host, port string, logMessages bool) (lib.MessageReceived, error) {
 	msgChannel := make(chan streaming.Message)
-	url := "http://" + host + ":" + port
+	url := host + ":" + port
 	ctx, cancel := context.WithCancel(context.Background())
 	NewConsumer(ctx, url, topic, msgChannel)
 	defer cancel()
